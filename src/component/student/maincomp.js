9b674503935f7aa2a50786fa61f70a8b7f2fba57
BasitAhmed <basitahmed1997@gmail.com>
2020-11-06 13:59:38 -0800

    Student Page

--- src/component/student/maincomp.js
+++ src/component/student/maincomp.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./student.css";
 import logo from "../../images/logo.png";
+import ReactPlayer from "react-player";
+import ReactLivestream from "react-livestream";
 
 export default class MainComp extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      mainBoard: true,
+      notification: false,
+      lectures: false,
+      books: false,
+      fees: false,
+    };
   }
   render() {
     return (
@@ -15,19 +23,173 @@ export default class MainComp extends React.Component {
             <div className="p-5">
               <img src={logo} width="100%" />
             </div>
-            <div className='p-5'>
-                <ul classNames='sideList'>
-                    <li>Messages</li>
-                    <li>Fees</li>
-                    <li>Videos</li>
-                    <li>Logout</li>
-                </ul>
+            <div className="p-5">
+              <ul className="list">
+                <li
+                  onClick={() =>
+                    this.setState({
+                      notification: false,
+                      books: false,
+                      fees: false,
+                      lectures: false,
+                      mainBoard: true,
+                    })
+                  }
+                >
+                  Main Menu
+                </li>
+                <li
+                  onClick={() =>
+                    this.setState({
+                      notification: true,
+                      books: false,
+                      fees: false,
+                      lectures: false,
+                      mainBoard: false,
+                    })
+                  }
+                >
+                  Notification
+                </li>
+                <li
+                  onClick={() =>
+                    this.setState({
+                      notification: false,
+                      books: false,
+                      fees: true,
+                      lectures: false,
+                      mainBoard: false,
+                    })
+                  }
+                >
+                  Fees
+                </li>
+                <li
+                  onClick={() =>
+                    this.setState({
+                      notification: false,
+                      books: false,
+                      fees: false,
+                      lectures: true,
+                      mainBoard: false,
+                    })
+                  }
+                >
+                  Videos
+                </li>
+                <li>Logout</li>
+              </ul>
             </div>
           </div>
-          <div
-            style={{ height: "100vh", overflow: "scroll" }}
-            className="col-md-9"
-          ></div>
+          <div className="col-md-9">
+            <div className="container">
+              <div className="top">
+                <p className="topText">
+                  <i class="fas fa-user-circle"></i> <span>Abdul Basit</span>
+                </p>
+              </div>
+              {this.state.mainBoard ? (
+                <div className="row">
+                  <div className="col-md-4">
+                    <div
+                      onClick={() =>
+                        this.setState({
+                          notification: true,
+                          books: false,
+                          fees: false,
+                          lectures: false,
+                          mainBoard: false,
+                        })
+                      }
+                      className="smallCard p-5 my-3"
+                    >
+                      <h1 className="color">
+                        <i class="fas fa-bell"></i>
+                      </h1>
+                      <h2>Notification</h2>
+                    </div>
+                  </div>
+                  <div className="col-md-4">
+                    <div
+                      onClick={() =>
+                        this.setState({
+                          notification: false,
+                          books: false,
+                          fees: true,
+                          lectures: false,
+                          mainBoard: false,
+                        })
+                      }
+                      className="smallCard p-5 my-3"
+                    >
+                      <h1 className="color">
+                        <i class="fas fa-money-check-alt"></i>
+                      </h1>
+                      <h2>Fees</h2>
+                    </div>
+                  </div>
+                  <div className="col-md-4">
+                    <div
+                      onClick={() =>
+                        this.setState({
+                          notification: false,
+                          books: false,
+                          fees: false,
+                          lectures: true,
+                          mainBoard: false,
+                        })
+                      }
+                      className="smallCard p-5 my-3"
+                    >
+                      <h1 className="color">
+                        <i class="fas fa-play-circle"></i>
+                      </h1>
+                      <h2>Lectures</h2>
+                    </div>
+                  </div>
+                  <div className="col-md-4">
+                    <div
+                      onClick={() =>
+                        this.setState({
+                          notification: false,
+                          books: true,
+                          fees: false,
+                          lectures: false,
+                          mainBoard: false,
+                        })
+                      }
+                      className="smallCard p-5 my-3"
+                    >
+                      <h1 className="color">
+                        <i class="fas fa-book"></i>
+                      </h1>
+                      <h2>Books</h2>
+                    </div>
+                  </div>
+                </div>
+              ) : null}
+              {this.state.lectures ? (
+                <div className="video">
+                  <p style={{ fontSize: "3em" }}>Video Will Appear Here</p>
+                </div>
+              ) : null}
+              {this.state.notification ? (
+                <div className="notification top p-5">
+                  <h2>Notification</h2>
+                </div>
+              ) : null}
+              {this.state.books ? (
+                <div className="books top p-5">
+                  <h2>Books</h2>
+                </div>
+              ) : null}
+              {this.state.fees ? (
+                <div className="fees top p-5">
+                  <h2>Fees</h2>
+                </div>
+              ) : null}
+            </div>
+          </div>
         </div>
       </div>
     );
